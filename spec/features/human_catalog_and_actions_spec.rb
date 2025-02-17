0d41d5b141ebca8f64f3e266e5bedaa156fa5599
thotmx <hermes.o.r@gmail.com>
2025-02-17 00:02:28 -0600

    Add more unit tests

--- spec/features/human_catalog_and_actions_spec.rb
+++ spec/features/human_catalog_and_actions_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Creating a human', type: :feature do
   scenario 'User creates a human' do
     visit root_path
     click_on 'Create a new human'
-    expect(page).to have_content('Back to humans')
+    expect(page).to have_content('Human Catalog')
     expect(page).to have_content('Creating a human')
     fill_in 'Name', with: 'John Doe'
     click_on 'Create Human'
@@ -17,13 +17,21 @@ RSpec.describe 'Creating a human', type: :feature do
     Human.create(name: 'John Doe', human_type: 'mutant', dna: "AAAAAA\nAAAAAA\nAAAAAA\nAAAAAA\nAAAAAA\nAAAAAA")
     visit root_path
     click_on 'Alter human'
-    expect(page).to have_content('Back to humans')
+    expect(page).to have_content('Human Catalog')
     expect(page).to have_content('Altering this human')
     fill_in 'Name', with: 'John Wick'
-    click_on 'Modify Human'
+    click_on 'Update Human'
     visit humans_path
     expect(page).to have_content('John Wick')
     expect(page).to have_content('mutant')
   end
 
+  scenario 'User destroy already created human' do
+    Human.create(name: 'John Doe', human_type: 'mutant', dna: "AAAAAA\nAAAAAA\nAAAAAA\nAAAAAA\nAAAAAA\nAAAAAA")
+    visit root_path
+    click_on 'Destroy human'
+    visit humans_path
+    expect(page).not_to have_content('John Doe')
+    expect(page).not_to have_content('mutant')
+  end
 end
--- spec/models/human_spec.rb
+++ spec/models/human_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Human, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should have a name' do
+    human = Human.new(name: '')
+    expect(human).not_to be_valid
+  end
+
+  it 'should have a dna' do
+    human = Human.new(name: 'Test')
+    expect(human).not_to be_valid
+  end
+
+  it 'save a valid human' do
+    human = Human.new(name: 'Test', dna: "AAAAAA\nAAAAAA\nAAAAAA\nAAAAAA\nAAAAAA\nAAAAAA")
+    expect(human.save).to be true
+    expect(human.human_type).to eq("mutant")
+  end
+
+  describe "dna_matrix_to_string" do
+    it "should convert a matrix to a string" do
+      matrix = {
+        0 => { 0 => "A", 1 => "A", 2 => "A", 3 => "A", 4 => "A", 5 => "A" },
+        1 => { 0 => "A", 1 => "A", 2 => "A", 3 => "A", 4 => "A", 5 => "A" },
+        2 => { 0 => "A", 1 => "A", 2 => "A", 3 => "A", 4 => "A", 5 => "A" },
+        3 => { 0 => "A", 1 => "A", 2 => "A", 3 => "A", 4 => "A", 5 => "A" },
+        4 => { 0 => "A", 1 => "A", 2 => "A", 3 => "A", 4 => "A", 5 => "A" },
+        5 => { 0 => "A", 1 => "A", 2 => "A", 3 => "A", 4 => "A", 5 => "A" }
+      }
+      expect(Human.dna_matrix_to_string(matrix)).to eq("AAAAAA\nAAAAAA\nAAAAAA\nAAAAAA\nAAAAAA\nAAAAAA\n")
+    end
+  end
 end
